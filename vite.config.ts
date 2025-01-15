4cb25279e2519d8eb5c9115df3985b67ca3e3a78
ITboo <annaacdc@tut.by>
2025-01-15 19:18:18 +0300

    init: bootstrap with vite and vite-pwa-plugin

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate', workbox:{
+    globPatterns:['**/*.{html, css, js, ico, png, svg}']
+  } })],
+})
