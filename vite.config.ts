4ea8f950d52edfe236367a4ad22ae2b36a510ec5
ITboo <annaacdc@tut.by>
2025-01-15 20:00:02 +0300

    chore: add manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { ManifestOptions, VitePWA } from "vite-plugin-pwa";
+
+const manifest: Partial<ManifestOptions> = {
+  theme_color: "#000000",
+  background_color: "#000000",
+  icons: [
+    {
+      purpose: "maskable",
+      sizes: "512x512",
+      src: "icon512_maskable.png",
+      type: "image/png",
+    },
+    {
+      purpose: "any",
+      sizes: "512x512",
+      src: "icon512_rounded.png",
+      type: "image/png",
+    },
+  ],
+  orientation: "any",
+  display: "standalone",
+  lang: "ru-RU",
+  name: "pwa",
+  short_name: "pwa",
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate', workbox:{
-    globPatterns:['**/*.{html, css, js, ico, png, svg}']
-  } })],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{html, css, js, ico, png, svg}"],
+      },
+      manifest: manifest
+    }),
+  ],
+});
